336a568769c4d73cd844db693bbbc98feba29a24
Jean Mizero <mizero2@gmail.com>
2023-01-01 20:12:55 -0700

    Create index.py
    PE1 Module 2

---
+++ Python-Ensentials-1/index.py
@@ -0,0 +1,10 @@
+# Python is an interpreted language. This means that it inherits all the described advantages and disadvantages. Of course, it adds some of its unique features to both sets.
+# Why Python? it's easy to learn, it's easy to teach,it's easy to use for writing new software, it's easy to understand and it's easy to obtain, install and deploy
+# Why not Python? low-level programming (sometimes called "close to metal" programming): if you want to implement an extremely effective driver or graphical engine, you wouldn't use Python and applications for mobile devices: although this territory is still waiting to be conquered by Python, it will most likely happen someday
+# Python implementations
+## Python Software Foundation [https://www.python.org/psf-landing/]
+### PE1: Module 2. Python Data Types, Variables, Operators, and Basic I/O Operations
+# 2.4 Strings
+print('I like "Monty Python"')
+print('I like "Monty Python"')
+### PE1: Module 3. Boolean Values, Conditional Execution, Loops, Lists and List Processing, Logical and Bitwise Operations
